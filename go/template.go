3c7400af3137ab041530fe75cb690dc078ad4c6d
Mark Vu <markvu2607@Macbook-Air-M2.local>
2024-09-09 16:56:00 +0700

    add template go

--- go/template.go
+++ go/template.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+)
+
+/* --------------- Write below --------------- */
+func fn() string {
+	return "Hello world"
+}
+
+func main() {
+	// Read input
+
+	// Get the result and print it
+	result := fn()
+	fmt.Println(result)
+}
